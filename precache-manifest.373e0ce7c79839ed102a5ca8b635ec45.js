19d47c229e357c74713b1e9b1956057e49dbb13e
dandotwave <danielcarvajal1295@gmail.com>
2020-09-18 18:54:03 -0400

    Updates

---
+++ precache-manifest.373e0ce7c79839ed102a5ca8b635ec45.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2547d09979195cf1e2bea17a9ef722b7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5ef0c8dc529beabd7b25",
+    "url": "/portfolio/static/css/main.1e91dc95.chunk.css"
+  },
+  {
+    "revision": "39324e3441b7b5eadbb1",
+    "url": "/portfolio/static/js/2.0c142734.chunk.js"
+  },
+  {
+    "revision": "e37227d42ad099732402c7f0ce512cee",
+    "url": "/portfolio/static/js/2.0c142734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef0c8dc529beabd7b25",
+    "url": "/portfolio/static/js/main.33b9710f.chunk.js"
+  },
+  {
+    "revision": "f0d302f9291e1b1fdb7b",
+    "url": "/portfolio/static/js/runtime-main.ec75c94c.js"
+  },
+  {
+    "revision": "73995e890b2b0f573ccaa2ad8765e4d5",
+    "url": "/portfolio/static/media/Avenir85Heavy.73995e89.ttf"
+  },
+  {
+    "revision": "421921161fe8d7fa1c1d44b40dd33d67",
+    "url": "/portfolio/static/media/Avenir_35_Light.42192116.ttf"
+  },
+  {
+    "revision": "788f9cc4e37cde7847cd42d30bee07b7",
+    "url": "/portfolio/static/media/Barlow-Medium.788f9cc4.ttf"
+  },
+  {
+    "revision": "5d3c4a34c8744610e421024f21b2cea5",
+    "url": "/portfolio/static/media/EurostileBold.5d3c4a34.ttf"
+  },
+  {
+    "revision": "87ab1444f79438feb22565e234655f0d",
+    "url": "/portfolio/static/media/avataaars.87ab1444.svg"
+  },
+  {
+    "revision": "ed45d35bc27703c668f5daa22166eec1",
+    "url": "/portfolio/static/media/avataaars_2.ed45d35b.svg"
+  },
+  {
+    "revision": "2e1c7ae4254f7138732a712f6ed8e640",
+    "url": "/portfolio/static/media/avataaars_3.2e1c7ae4.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4c1df7071d3fd444ae5f3ee36b8a3581",
+    "url": "/portfolio/static/media/never-1 copy2.4c1df707.jpg"
+  },
+  {
+    "revision": "8dd2b392f41c5ea5c2fa182b6b48b27a",
+    "url": "/portfolio/static/media/never-1 copy3v2.8dd2b392.jpg"
+  },
+  {
+    "revision": "0286f2dc9d5fe22be8b75a000d595e34",
+    "url": "/portfolio/static/media/never-1 copy4.0286f2dc.jpg"
+  },
+  {
+    "revision": "c772a833f4898768e74f75b7294bb770",
+    "url": "/portfolio/static/media/never-1 copy5.c772a833.jpg"
+  },
+  {
+    "revision": "c8ebb0f2d369ac5fa067c9afd52d642c",
+    "url": "/portfolio/static/media/never-1 copy6.c8ebb0f2.jpg"
+  }
+]);
